f9baf0096c49d8096a812597c00d907e88180680
Tetiana Korshikova <t.korshikova@gmail.com>
2020-01-29 02:56:35 +0200

    дÐhomework 2 part 2

---
+++ 2.2.py
@@ -0,0 +1,20 @@
+import random
+
+A=(random.randint(0,10))
+B=(random.randint(0,10))
+C=(random.randint(0,10))
+
+if A>B:
+    print("Я обожаю Питон")
+elif A<B:
+    print("Я ненавижу Питон")
+else: print (C,"Теперь эта")
+if A + B < C:
+    print("Какое счастье!")
+elif A + B > C:
+    print("Какая печаль!")
+else:
+    print("Страдание")
+
+
+
