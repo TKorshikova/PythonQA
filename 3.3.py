3e0377c16b9b69dcf023aac1ff881a752ab4ddf3
Tetiana Korshikova <t.korshikova@gmail.com>
2020-02-06 20:20:44 +0200

    Домашнее задание 3.3 Ключ-значение.

--- 3.3.py
+++ 3.3.py
@@ -2,7 +2,7 @@ import random
 
 
 def key_value(username, minimum, maximum):
-    return {user: random.randint(minimum, maximum)}
+    return {username: random.randint(minimum, maximum)}
 
 
 user = str(input("Введите вашу фамилию : "))
